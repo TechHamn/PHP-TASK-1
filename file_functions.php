31fa00cb53a53299708fd3b84e2d3562584a7c40
zmoghbel <48296807+zmoghbel@users.noreply.github.com>
2022-01-21 12:04:26 +0330

    Add Comments

--- file_functions.php
+++ file_functions.php
@@ -1,18 +1,27 @@
 <?php
+/* 
+* This file includes functions related by reding text file and converting it to array 
+* Created By Zeinab Moghbel
+* On 2022-01-19
+*/
 
+# Read the text file
 function read_file($filename){
     return file_get_contents($filename);
 }
 
+# Read the first word of the lind
 function firstLine($line){
     return substr(trim($line), 0, 1);
 }
 
+# Separate config from value in the line
 function setData($line){
     $arrData = explode("=",$line);
     return $arrData;
 }
 
+# Recursive Function for getting the output array
 function recursiveArr($arr, $step, $cArr, $value){
         if($step == count($cArr)) return ((is_numeric($value)) ? $cArr=$value : $cArr=$value);
         $arr[$cArr[$step]] = recursiveArr($arr[$cArr[$step]],++$step,$cArr,$value);
