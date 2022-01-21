31fce033a22bae1a18efc448836308668b544029
zmoghbel <48296807+zmoghbel@users.noreply.github.com>
2022-01-21 12:03:11 +0330

    Add Comments

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
 <?php
+/* 
+* This file includes functions related by reding text file and converting it to array 
+* Created By Zeinab Moghbel
+* On 2022-01-19
+*/
+
 require_once('file_functions.php');
 error_reporting(E_ERROR | E_PARSE);
 
-$filename = 'config.txt';
+$filename = 'config.txt'; // the text file
 
 if(file_exists($filename)){
-    $filetext = nl2br(read_file($filename)).'<br>';
-    $fileArr = explode("<br />",$filetext);
+    $filetext = nl2br(read_file($filename)).'<br>'; //add br to end of each line
+    $fileArr = explode("<br />",$filetext); //Separate each line and insert it in array
 
     //var_dump($fileArr);
     
-    $dataConfigArr = null;
+    # The output array
+    $dataConfigArr = null; 
 
+    # for each line, we run several task
     foreach($fileArr as $key => $value){
         
         //echo $key . " - ". $value.'<br>';
         if(firstLine($value) !== '#' && firstLine($value) != null){
             $parsLine = setData($value);
-            $parseConfig = explode(".", $parsLine[0]);
-            $configLineValue = $parsLine[1];
+            $parseConfig = explode(".", $parsLine[0]); // The config part of line in array form
+            $configLineValue = $parsLine[1]; // The value part of line 
+
+            # Call recursive function to get result
             $dataConfigArr = recursiveArr($dataConfigArr, 0, $parseConfig, $configLineValue);
         }
         
